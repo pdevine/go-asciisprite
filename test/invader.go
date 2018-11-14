757187a127ff36e04e2d0bf901d92f6886fc4c4d
Patrick Devine <pdevine@sonic.net>
2018-11-13 16:52:34 -0800

    Space Invaders!

---
+++ test/invader.go
@@ -0,0 +1,529 @@
+package main
+
+import (
+	"time"
+
+	sprite "github.com/pdevine/go-asciisprite"
+	tm "github.com/gdamore/tcell/termbox"
+)
+
+var allSprites sprite.SpriteGroup
+var Width int
+var Height int
+var gameState *GameState
+
+type GameState struct {
+	invaders  []*Invader
+	direction int
+	nextFire  *NextInvaderToFire
+	player    *Fighter
+}
+
+type Invader struct {
+	sprite.BaseSprite
+	Timer     int
+	TimeOut   int
+	Exploding bool
+	Col	  int
+	Dead      bool
+}
+
+type NextInvaderToFire struct {
+	Invader *Invader
+	Timer   int
+	TimeOut int
+}
+
+type Ufo struct {
+	sprite.BaseSprite
+	Timer   int
+	TimeOut int
+}
+
+type Fighter struct {
+	sprite.BaseSprite
+	Timer     int
+	TimeOut   int
+	VX        float32
+	AX        float32
+	Exploding bool
+	Lives	  int
+}
+
+type Bullet struct {
+	sprite.BaseSprite
+	VY	int
+	Dead	bool
+}
+
+/*
+const invader_c0 = `  X     X
+ XXXXXXXXX
+ XX XXX XX
+ XXXXXXXXX
+ X X   X X
+X X   X X`
+
+const invader_c1 = `  X     X
+ XXXXXXXXX
+ XX XXX XX
+ XXXXXXXXX
+ X X   X X 
+  X X   X X`
+*/
+
+const invader_c0 = `  X     X   
+   X   X    
+  XXXXXXX   
+ XX XXX XX  
+XXXXXXXXXXX 
+X XXXXXXX X 
+X X     X X 
+   XX XX     `
+
+const invader_c1 = `  X     X   
+X  X   X  X 
+X XXXXXXX X 
+XXX XXX XXX 
+XXXXXXXXXXX 
+ XXXXXXXXX  
+  X     X   
+ X       X  `
+
+
+const invader2_c0 = `   XX     
+  XXXX   
+ XXXXXX  
+XX XX XX 
+XXXXXXXX 
+  X  X   
+ X XX X  
+X X  X X  `
+
+const invader2_c1 = `   XX    
+  XXXX   
+ XXXXXX  
+XX XX XX 
+XXXXXXXX 
+  X  X   
+ X    X  
+  X  X    `
+
+const invader3_c0 = `    XXXX   
+ XXXXXXXXXX  
+XXXXXXXXXXXX 
+XXX  XX  XXX 
+XXXXXXXXXXXX 
+   XX  XX    
+  X  XX  X   
+   X    X    `
+
+const invader3_c1 = `    XXXX   
+ XXXXXXXXXX  
+XXXXXXXXXXXX 
+XXX  XX  XXX 
+XXXXXXXXXXXX 
+   XX  XX    
+  XX XX XX   
+XX        XX `
+
+
+const fighter_c0 = `
+      X       
+     XXX      
+     XXX     
+ XXXXXXXXXXX  
+XXXXXXXXXXXXX
+XXXXXXXXXXXXX
+XXXXXXXXXXXXX`
+
+
+const fighter_explode_c0 = `
+   X
+     X X X   
+   X X       
+      XX XX  
+X   X XX X   
+  XXXXXXXX X 
+ XXXXXXXXXX X`
+
+const fighter_explode_c1 = `
+
+             
+    X        
+  X    X X X 
+X   X XX     
+  X XX X X X 
+ XX XXX XXX X`
+
+
+const explosion_c1 = `X  X   X  X  
+ X  X X  X   
+  X     X    
+XX       XX  
+  X     X    
+ X  X X  X   
+X  X   X  X 
+            `
+
+const explosion_c0 = `        
+         
+     XX  
+   X    X
+   X    X
+     XX  `
+
+
+const ufo_c0 = `
+     XXXXXXX      
+   XXXXXXXXXXX    
+  XXXXXXXXXXXXX   
+ X XXX XXX XXX X  
+XXXXXXXXXXXXXXXXX 
+  XXX  XXX  XXX   
+   X         X     `
+
+const ufo_c1 = `
+     XXXXXXX      
+   XXXXXXXXXXX    
+  XXXXXXXXXXXXX   
+ XXX XXX XXX XXX  
+XXXXXXXXXXXXXXXXX 
+  XXX  XXX  XXX   
+   X         X     `
+
+func (gs *GameState) Update() {
+	gs.cullSprites()
+	gs.checkDirection()
+	gs.nextFire.Update()
+}
+
+func (gs *GameState) checkDirection() {
+	changeDir := false
+	var d int
+	for _, s := range gs.invaders {
+		if s.X > 98 && gs.direction > 0 {
+			d = -(s.X - 98)
+			changeDir = true
+			break
+		} else if s.X < 3  && gs.direction < 0 {
+			d = 3 - s.X
+			changeDir = true
+			break
+		}
+	}
+	if changeDir {
+		for _, s := range gs.invaders {
+			s.X += d
+			s.Y += 3
+
+		}
+		gs.direction = -gs.direction
+	}
+}
+
+func (gs *GameState) cullSprites() {
+	for _, s := range allSprites.Sprites {
+		switch sp := s.(type) {
+		case *Bullet:
+			if sp.Dead == true {
+				allSprites.Remove(sp)
+			}
+		case *Invader:
+			if sp.Dead == true {
+				gs.removeInvader(sp)
+				allSprites.Remove(sp)
+			}
+		default:
+		}
+	}
+}
+
+func (gs *GameState) removeInvader(i *Invader) {
+	var idx int
+	for cnt, s := range gs.invaders {
+		if i == s {
+			idx = cnt
+			break
+		}
+	}
+	copy(gs.invaders[idx:], gs.invaders[idx+1:])
+	gs.invaders[len(gs.invaders)-1] = nil
+	gs.invaders = gs.invaders[:len(gs.invaders)-1]
+}
+
+func (gs *GameState) createWave() {
+	gs.direction = 2
+
+	for cnt := 0; cnt < 9; cnt++ {
+		i := NewInvader(1)
+		i.Col = cnt
+		i.X = cnt * 10 + 10
+		i.Y = 7
+		gs.invaders = append(gs.invaders, i)
+		allSprites.Sprites = append(allSprites.Sprites, i)
+	}
+
+	for y := 0; y < 2; y++ {
+		for cnt := 0; cnt < 9; cnt++ {
+	  		i := NewInvader(0)
+			i.Col = cnt
+			i.X = cnt * 10 + 9
+			i.Y = y * 6 + 13
+			gs.invaders = append(gs.invaders, i)
+			allSprites.Sprites = append(allSprites.Sprites, i)
+		}
+	}
+
+	for y := 0; y < 2; y++ {
+		for cnt := 0; cnt < 9; cnt++ {
+	  		i := NewInvader(2)
+			i.Col = cnt
+			i.X = cnt * 10 + 9
+			i.Y = y * 6 + 25
+			gs.invaders = append(gs.invaders, i)
+			allSprites.Sprites = append(allSprites.Sprites, i)
+		}
+	}
+
+	gs.nextFire = &NextInvaderToFire{
+		Invader: gs.invaders[len(gs.invaders)-1],
+		TimeOut: 5,
+	}
+}
+
+func NewInvader(t int) *Invader {
+	s := &Invader{BaseSprite: sprite.BaseSprite{
+		Visible:        true,
+		X:		2,
+		Y:              2,
+		CurrentCostume: 0},
+		Timer:   0,
+		TimeOut: 6}
+
+	if t == 0 {
+		s.AddCostume(sprite.Convert(invader_c0))
+		s.AddCostume(sprite.Convert(invader_c1))
+	} else if t == 1 {
+		s.AddCostume(sprite.Convert(invader2_c0))
+		s.AddCostume(sprite.Convert(invader2_c1))
+	} else if t == 2 {
+		s.AddCostume(sprite.Convert(invader3_c0))
+		s.AddCostume(sprite.Convert(invader3_c1))
+	}
+	return s
+}
+
+func (s *Invader) Explode() {
+	s.Costumes = []*sprite.Costume{}
+	s.AddCostume(sprite.Convert(explosion_c0))
+	s.Exploding = true
+}
+
+func (s *Invader) Update() {
+	s.Timer = s.Timer + 1
+	if s.Timer > s.TimeOut {
+		s.X = s.X + gameState.direction
+		s.CurrentCostume = s.CurrentCostume + 1
+		if s.CurrentCostume >= len(s.Costumes) {
+			s.CurrentCostume = 0
+			if s.Exploding {
+				s.Dead = true
+			}
+		}
+		s.Timer = 0
+	}
+}
+
+func (s *Invader) Fire() {
+	b := &Bullet{BaseSprite: sprite.BaseSprite{
+		Visible: true,
+		X: s.X+3,
+		Y: s.Y+4},
+		VY: 2}
+	b.AddCostume(sprite.Convert("X \n X\nX \n  "))
+	b.AddCostume(sprite.Convert(" X\nX \n X\n  "))
+	allSprites.Sprites = append(allSprites.Sprites, b)
+}
+
+func (n *NextInvaderToFire) Update() {
+	n.Timer = n.Timer + 1
+	if n.Timer > n.TimeOut {
+		n.Invader.Fire()
+		n.Timer = 0
+	}
+}
+
+func NewUfo() *Ufo {
+	s := &Ufo{BaseSprite: sprite.BaseSprite{
+		Visible:	true,
+		X: 2,
+		Y: 2},
+		TimeOut: 3}
+	s.AddCostume(sprite.Convert(ufo_c0))
+	s.AddCostume(sprite.Convert(ufo_c1))
+	return s
+}
+
+func (s *Ufo) Update() {
+	s.Timer = s.Timer + 1
+	if s.Timer > s.TimeOut {
+		s.CurrentCostume = s.CurrentCostume + 1
+		if s.CurrentCostume >= len(s.Costumes) {
+			s.CurrentCostume = 0
+		}
+		s.Timer = 0
+	}
+}
+
+
+func NewFighter() *Fighter {
+	s := &Fighter{BaseSprite: sprite.BaseSprite{
+		Visible:	true,
+		X: 2,
+		Y: 70},
+		TimeOut: 2,
+		}
+	s.AddCostume(sprite.Convert(fighter_c0))
+	return s
+}
+
+func (s *Fighter) MoveLeft() {
+	s.AX -= 1
+}
+
+func (s *Fighter) MoveRight() {
+	s.AX += 1
+} 
+
+func (s *Fighter) Fire() {
+	b := &Bullet{BaseSprite: sprite.BaseSprite{
+		Visible: true,
+		X: s.X+3,
+		Y: s.Y},
+		VY: -2}
+	b.AddCostume(sprite.Convert("X \n  "))
+	allSprites.Sprites = append(allSprites.Sprites, b)
+}
+
+func (s *Fighter) Update() {
+	s.VX = s.VX + s.AX
+	s.AX = 0
+	s.X += int(s.VX)
+
+	if s.X < 2 {
+		s.X = 2
+		s.VX = 0
+		s.AX = 0
+	} else if s.X > 100 {
+		s.X = 100
+		s.VX = 0
+		s.AX = 0
+	}
+
+	if s.Exploding {
+		s.Timer += 1
+		if s.Timer > s.TimeOut {
+			s.CurrentCostume = s.CurrentCostume + 1
+			if s.CurrentCostume >= len(s.Costumes) {
+				s.CurrentCostume = 0
+			}
+			s.Timer = 0
+		}
+	}
+}
+
+func (s *Fighter) Explode() {
+	s.Costumes = []*sprite.Costume{}
+	s.AddCostume(sprite.Convert(fighter_explode_c0))
+	s.AddCostume(sprite.Convert(fighter_explode_c1))
+	s.Exploding = true
+}
+
+func (s *Bullet) Update() {
+	s.Y += s.VY
+
+	if s.Y < 0 || s.Y > 80 {
+		s.Dead = true
+		return
+	}
+
+	s.CurrentCostume = s.CurrentCostume + 1
+	if s.CurrentCostume >= len(s.Costumes) {
+		s.CurrentCostume = 0
+	}
+
+	if gameState.player.HitAtPoint(s.X, s.Y+1) {
+		gameState.player.Explode()
+	}
+
+	for _, i := range gameState.invaders {
+		if !i.Exploding && i.HitAtPoint(s.X, s.Y) {
+			i.Explode()
+			s.Dead = true
+		}
+	}
+
+}
+
+
+func main() {
+	// XXX - Wait a bit until the terminal is properly initialized
+	time.Sleep(500 * time.Millisecond)
+
+	err := tm.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer tm.Close()
+
+	Width, Height = tm.Size()
+
+	event_queue := make(chan tm.Event)
+	go func() {
+		for {
+			event_queue <- tm.PollEvent()
+		}
+	}()
+
+	//ufo := NewUfo()
+	//ufo.X = 24
+	//allSprites.Sprites = append(allSprites.Sprites, ufo)
+	//invaders = append(invaders, ufo)
+
+	gameState = &GameState{player: NewFighter(),}
+	gameState.player.X = 34
+	allSprites.Sprites = append(allSprites.Sprites, gameState.player)
+
+
+mainloop:
+	for {
+		tm.Clear(tm.ColorDefault, tm.ColorDefault)
+
+		select {
+		case ev := <-event_queue:
+			if ev.Type == tm.EventKey {
+				if ev.Key == tm.KeyEsc || ev.Ch == 'q' {
+					break mainloop
+				} else if ev.Key == tm.KeyArrowLeft {
+					gameState.player.MoveLeft()
+				} else if ev.Key == tm.KeyArrowRight {
+					gameState.player.MoveRight()
+				} else if ev.Key == tm.KeySpace {
+					gameState.player.Fire()
+				}
+			} else if ev.Type == tm.EventResize {
+				Width = ev.Width
+				Height = ev.Height
+			}
+		default:
+			if len(gameState.invaders) == 0 {
+				gameState.createWave()
+			}
+			allSprites.Update()
+			gameState.Update()
+			allSprites.Render()
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
